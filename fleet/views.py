8399c20ab88c6456b2859327f27ec06741ca4784
David Onuh <davidonuh1@gmail.com>
2022-01-30 21:11:57 +0100

    Add test cases for flight api viewsets Install black for linting. run linter on fleet app

--- fleet/views.py
+++ fleet/views.py
@@ -11,7 +11,6 @@ class AirportViewset(viewsets.ModelViewSet):
     serializer_class = AirportSerializer
 
 
-
 class AircraftViewset(viewsets.ModelViewSet):
 
     queryset = Aircraft.objects.all()
@@ -22,14 +21,14 @@ class FlightViewset(viewsets.ModelViewSet):
 
     queryset = Flight.objects.all()
     serializer_class = FlightSerializer
-    filterset_fields = ['departure_airport', 'arrival_airport']
+    filterset_fields = ["departure_airport", "arrival_airport"]
 
     def get_queryset(self):
         # overide original queryset to add filter by departure timerange
         queryset = super().get_queryset()
 
-        start = self.request.query_params.get('start')
-        end = self.request.query_params.get('end')
+        start = self.request.query_params.get("start")
+        end = self.request.query_params.get("end")
 
         if not start or not end:
             return queryset
