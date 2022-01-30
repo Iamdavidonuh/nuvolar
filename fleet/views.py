bbf88e8297c7fe02512ecbc94a8946ad811446cb
David Onuh <davidonuh1@gmail.com>
2022-01-30 07:33:36 +0100

    add filter by departure and arrival airport, and filter by departure_time range

--- fleet/views.py
+++ fleet/views.py
@@ -22,3 +22,15 @@ class FlightViewset(viewsets.ModelViewSet):
 
     queryset = Flight.objects.all()
     serializer_class = FlightSerializer
+    filterset_fields = ['departure_airport', 'arrival_airport']
+
+    def get_queryset(self):
+        # overide original queryset to add filter by departure timerange
+        queryset = super().get_queryset()
+
+        start = self.request.query_params.get('start')
+        end = self.request.query_params.get('end')
+
+        if not start or not end:
+            return queryset
+        return queryset.filter(departure_time__range=[start, end])
